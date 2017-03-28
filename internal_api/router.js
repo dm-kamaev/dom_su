3ed540b9522bc35a718765dcc3270214e99c3902
Овсянников Руслан <ovsyannikov.r@domovenok.su>
2017-03-28 18:43:10 +0300

    recovery router 2

--- internal_api/router.js
+++ internal_api/router.js
@@ -2,14 +2,14 @@
 
 const logger = require('logger')(module)
 const Router = require('koa-router');
-const internalServiceAPI = new Router();
+const internalAPI = new Router();
 
 const Secret = "1Ac0uGgbLLA6eUpkV4gh"
 
 const { models, ErrorCodes, ModelsError } = require('models')
 const { Phone } = models
 
-internalServiceAPI.post('/service/modification', async function (ctx, next) {
+internalAPI.post('/service/modification', async function (ctx, next) {
     /*
     [
         {
@@ -70,4 +70,4 @@ internalServiceAPI.post('/service/modification', async function (ctx, next) {
 })
 
 
-module.exports = { internalServiceAPI }
+module.exports = { internalAPI }
