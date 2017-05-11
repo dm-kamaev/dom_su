063c23dcb217f540165389d3e6297dc7687e2309
Овсянников Руслан <ovsyannikov.r@domovenok.su>
2017-05-11 18:17:15 +0300

    change payments to KSD

--- payments/router.js
+++ payments/router.js
@@ -16,14 +16,14 @@ let regExpAmount = new RegExp(/^(:?\d+)((\.|\,)(:?\d{1,2}))?$/, 'g')
 const URL_TINKOFF = "securepay.tinkoff.ru"
 
 // KSD
-// const TERMINAL_KEY = 'domovenok3DS'
-// const PASSWORD = '1AOJ.Di8b8EwGt,X'
+const TERMINAL_KEY = 'domovenok3DS'
+const PASSWORD = '1AOJ.Di8b8EwGt,X'
 // TEST KSD
 // PASSWORD = 'q6YYOi^^jsTo@l1S'
 
 // IP Ipatova
-const TERMINAL_KEY = '1487066466356'
-const PASSWORD = 'f7ydxrgo3c42l9ub'
+// const TERMINAL_KEY = '1487066466356'
+// const PASSWORD = 'f7ydxrgo3c42l9ub'
 
 // TEST IP Ipatovagi
 // const TERMINAL_KEY = '1487066466356DEMO'
