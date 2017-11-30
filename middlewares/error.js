921e8ea4e1d26c07d0f05e944a8d59ec03f3a998
Dmitrii K <dm-kamaev@rambler.ru>
2017-11-30 11:45:24 +0300

    DEVELOP-2234:  add logger for pancake

--- middlewares/error.js
+++ middlewares/error.js
@@ -2,6 +2,7 @@
 const fs = require('fs-promise')
 const logger = require('logger')(module)
 const { getTemplate, loadTemplate } = require('utils')
+const log = require('/p/pancake/lib/logger.js');
 
 let template404Opt = {
     path: 'templates/error/404.html',
@@ -18,24 +19,25 @@ loadTemplate(template500Opt)
 
 
 async function errorMiddleware(ctx, next) {
-        try {
-            await next()
-        } catch (err) {
-            let errorHtml
-            if (err.status === 404) {
-                //logger.error(`404 Path ${ctx.request.href} ${(ctx.headers.referer) ? '| referer ' + ctx.headers.referer : '' } `)
-                ctx.type = 'text/html'
-                ctx.status = 404
-                errorHtml = getTemplate(template404Opt)
-                ctx.body = errorHtml(ctx.proc({}))
-            } else {
-                logger.error(err)
-                ctx.type = 'text/html'
-                ctx.status = 500
-                errorHtml = getTemplate(template500Opt)
-                ctx.body = errorHtml(ctx.proc({}))
-            }
-        }
+  try {
+    await next()
+  } catch (err) {
+    log.warn(err);
+    let errorHtml;
+    if (err.status === 404) {
+      //logger.error(`404 Path ${ctx.request.href} ${(ctx.headers.referer) ? '| referer ' + ctx.headers.referer : '' } `)
+      ctx.type = 'text/html'
+      ctx.status = 404
+      errorHtml = getTemplate(template404Opt)
+      ctx.body = errorHtml(ctx.proc({}))
+    } else {
+      logger.error(err)
+      ctx.type = 'text/html'
+      ctx.status = 500
+      errorHtml = getTemplate(template500Opt)
+      ctx.body = errorHtml(ctx.proc({}))
+    }
+  }
 }
 
 module.exports = {errorMiddleware: errorMiddleware}
