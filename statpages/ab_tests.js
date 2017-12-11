472106a32b061da95f6d90b7a88f7e640bb6a497
Dmitrii K <dm-kamaev@rambler.ru>
2017-12-11 16:27:59 +0300

    A/B test

--- statpages/ab_tests.js
+++ statpages/ab_tests.js
@@ -6,10 +6,10 @@ const ABTestContainer = {
         //     {name: "control", page: 'generalnaya_uborka', ratio: 50, description: "Основные дополнительные услуги"},
         //     {name: "variation", page: 'generalnaya_uborka_ab', ratio: 50, description: "Мытье окон как доп. услуга"},
         // ]},
-        // 'podderzhka': { name: "", key: "podderzhka_6_wet_cleaning", forNewUser: true, variations: [
-        //     {name: "control", page: 'podderzhka', ratio: 50, description: "Основная"},
-        //     {name: "variation", page: 'podderzhka_ab', ratio: 50, description: "Влажная уборка"}
-        // ]}
+        'podderzhka': { name: "", key: "podderzhka_7_wet_cleaning", forNewUser: true, variations: [
+            {name: "control", page: 'podderzhka', ratio: 50, description: "Основная"},
+            {name: "variation", page: 'podderzhka_ab', ratio: 50, description: "Влажная уборка"}
+        ]}
         // 'posle_remonta': { name: "", key: "posle_remonta_2_new_design", forNewUser: true, variations: [
         //     {name: "control", page: 'posle_remonta', ratio: 50, description: "Основная"},
         //     {name: "variation", page: 'posle_remonta_ab', ratio: 50, description: "Пробуем новый дизайн"}
@@ -18,26 +18,26 @@ const ABTestContainer = {
         //     {name: "control", page: 'mite_okon', ratio: 50, description: "Основная"},
         //     {name: "variation", page: 'mite_okon_ab', ratio: 50, description: "Пробуем новый дизайн"}
         // ]},
-        'main': { name: "", key: "main_4_menu", forNewUser: true, variations: [
-            {name: "control", page: 'main', ratio: 50, description: "Основная"},
-            {name: "variation", page: 'main_ab', ratio: 50, description: "новое меню"}
-        ]},
-        'price__': {
-            name: "Изменение дизайна",
-            key: "price_new_design",
-            forNewUser: true,
-            variations: [{
-                name: "control",
-                page: 'price__',
-                ratio: 50,
-                description: "Основная"
-            }, {
-                name: "variation",
-                page: 'price_ab',
-                ratio: 50,
-                description: "Пробуем новый дизайн"
-            }]
-        },
+        // 'main': { name: "", key: "main_4_menu", forNewUser: true, variations: [
+        //     {name: "control", page: 'main', ratio: 50, description: "Основная"},
+        //     {name: "variation", page: 'main_ab', ratio: 50, description: "новое меню"}
+        // ]},
+        // 'price__': {
+        //     name: "Изменение дизайна",
+        //     key: "price_new_design",
+        //     forNewUser: true,
+        //     variations: [{
+        //         name: "control",
+        //         page: 'price__',
+        //         ratio: 50,
+        //         description: "Основная"
+        //     }, {
+        //         name: "variation",
+        //         page: 'price_ab',
+        //         ratio: 50,
+        //         description: "Пробуем новый дизайн"
+        //     }]
+        // },
     }
 }
 
