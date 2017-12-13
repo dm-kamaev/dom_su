0b743fd1798ec761eab4bb962275db5bb68600eb
Dmitrii K <dm-kamaev@rambler.ru>
2017-12-13 11:02:31 +0300

    DEVELOP-2169: add http header App-Version

--- app.js
+++ app.js
@@ -76,16 +76,19 @@ async function run() {
       const { request: req, response: res } = ctx;
       logger.log('HEADERS= '+JSON.stringify(req.headers, null, 2));
       if (req.method === 'POST' || req.method === 'GET') {
-        res.set("Access-Control-Allow-Origin", "*");
-        res.set("Access-Control-Allow-Credentials", "true");
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set('Access-Control-Allow-Credentials', 'true');
       }
       if (req.method === 'OPTIONS') {
         logger.log('set header for OPTIONS');
-        res.set("Access-Control-Allow-Origin", "*");
-        res.set("Access-Control-Allow-Headers", "status, a, b, X-Dom-Auth, Accept-Encoding, Accept, Accept-Language, Overwrite, Destination, Content-Type, content-type, Connection, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Pragma, User-Agent, Referer");
-        res.set("Access-Control-Allow-Credentials", "true");
-        res.set("Access-Control-Allow-Method", "PROPFIND, PROPPATCH, COPY, MOVE, DELETE, MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, OPTIONS, GET, POST");
-        res.set("Access-Control-Max-Age", "86400");
+        res.set('Access-Control-Allow-Origin', '*');
+        // App-Version –– current version for mobile
+        // status, a, b, –– auth cookie
+        // X-Dom-Auth –– uuid for auth and etc
+        res.set('Access-Control-Allow-Headers', 'App-Version, status, a, b, X-Dom-Auth, Accept-Encoding, Accept, Accept-Language, Overwrite, Destination, Content-Type, content-type, Connection, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Pragma, User-Agent, Referer');
+        res.set('Access-Control-Allow-Credentials', 'true');
+        res.set('Access-Control-Allow-Method', 'PROPFIND, PROPPATCH, COPY, MOVE, DELETE, MKCOL, LOCK, UNLOCK, PUT, GETLIB, VERSION-CONTROL, CHECKIN, CHECKOUT, UNCHECKOUT, REPORT, UPDATE, CANCELUPLOAD, HEAD, OPTIONS, GET, POST');
+        res.set('Access-Control-Max-Age', '86400');
         ctx.status = 200;
         return;
       }
