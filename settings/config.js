4f4a57e78d4573120c7e24f6f20189f05cd207df
Dmitrii K <dm-kamaev@rambler.ru>
2017-12-20 12:56:54 +0300

    DEVELOP-2399: add dynamic config

---
+++ settings/config.js
@@ -0,0 +1,64 @@
+#!/usr/local/bin/node
+
+'use strict';
+
+// КОНФИГУРАЦИОННЫЙ ФАЙЛ
+
+const env = require('/p/env/node_env.js');
+
+if (env !== 'dev' && env !== 'dev2' && env !== 'dev3' && env !== 'prod') {
+  throw new Error('Not valid env for node js "'+env+'"');
+}
+
+const CONF = {
+  env,
+  pg: {
+    user: 'domovenok',
+    password: 'domovenokPG',
+    database: 'pancake',
+    host: 'localhost'
+  },
+  api1C: {
+    ip: '192.168.1.63', // Саша
+    url: '/domovenok/hs/api/v2/',
+    oldAPI: '/domovenok/hs/api/',
+    port: 80,
+    ticket_url: '/domovenok/hs/rq'
+  },
+  // 'api1C': { // Лиза
+  //   'ip': '192.168.1.144',
+  //   'url': '/domovenok/hs/api/v2/',
+  //   'port': 80,
+  //   'ticket_url': '/domovenok/hs/rq'
+  // }
+  // api1C: { // Не работающий прод
+  //   ip: '192.168.1.241',
+  //   port: 80,
+  //   url: '/domovenok/hs/api/v2/',
+  // }
+  analytics: {
+    google: 'UA-91645230-1'
+  }
+};
+
+module.exports = function() {
+  if (env === 'prod') {
+    CONF.pg = {
+      user: 'domovenok',
+      password: 'TQ7Ee3q74F6hPNfp',
+      database: 'domovenok',
+      host: 'localhost'
+    };
+    CONF.api1C = {
+      ip: '192.168.2.4', // prod
+      url: '/domovenok/hs/api/v2/',
+      oldAPI: '/domovenok/hs/api/',
+      port: 80,
+      ticket_url: '/domovenok/hs/rq'
+    };
+    CONF.analytics = {
+      google: 'UA-26472404-10'
+    };
+  }
+  return CONF;
+}();
