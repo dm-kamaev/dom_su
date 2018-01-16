378c6ffa5965020c1f643154f168ca3ac772be49
Dmitrii K <dm-kamaev@rambler.ru>
2018-01-16 11:33:45 +0300

    DEVELOP-2482: add dynamic config for api1C

--- settings/config.js
+++ settings/config.js
@@ -32,13 +32,13 @@ const CONF = {
   //   'port': 80,
   //   'ticket_url': '/domovenok/hs/rq'
   // },
-  api1C: { // Паша
-    ip: '192.168.1.50',
-    url: '/domovenok/hs/api/v2/',
-    oldAPI: '/domovenok/hs/api/',
-    port: 80,
-    ticket_url: '/domovenok/hs/rq'
-  },
+  // api1C: { // Паша
+  //   ip: '192.168.1.50',
+  //   url: '/domovenok/hs/api/v2/',
+  //   oldAPI: '/domovenok/hs/api/',
+  //   port: 80,
+  //   ticket_url: '/domovenok/hs/rq'
+  // },
   // api1C: { // Не работающий прод
   //   ip: '192.168.1.241',
   //   port: 80,
@@ -50,28 +50,76 @@ const CONF = {
 };
 
 module.exports = function() {
-  if (env === 'dev') {
-    CONF.domain = 'https://www.dev.domovenok.su';
-  } else if (env === 'dev2') {
-    CONF.domain = 'https://www.dev2.domovenok.su';
-  } else if (env === 'prod') {
-    CONF.domain = 'https://www.domovenok.su';
-    CONF.pg = {
-      user: 'domovenok',
-      password: 'TQ7Ee3q74F6hPNfp',
-      database: 'domovenok',
-      host: 'localhost'
-    };
-    CONF.api1C = {
-      ip: '192.168.2.4', // prod
-      url: '/domovenok/hs/api/v2/',
-      oldAPI: '/domovenok/hs/api/',
-      port: 80,
-      ticket_url: '/domovenok/hs/rq'
-    };
-    CONF.analytics = {
-      google: 'UA-26472404-10'
-    };
+  switch (env) {
+    case 'dev':
+      CONF.domain = 'https://www.dev.domovenok.su';
+      CONF.api1C = get_api_1c('sasha');
+      break;
+    case 'dev2':
+      CONF.domain = 'https://www.dev2.domovenok.su';
+      CONF.api1C = get_api_1c('sasha');
+      break;
+    case 'prod':
+      CONF.domain = 'https://www.domovenok.su';
+      CONF.pg = {
+        user: 'domovenok',
+        password: 'TQ7Ee3q74F6hPNfp',
+        database: 'domovenok',
+        host: 'localhost'
+      };
+      CONF.api1C = get_api_1c('prod');
+      CONF.analytics = {
+        google: 'UA-26472404-10'
+      };
+      break;
+    default:
+      throw new Error('Not valid env for node js "'+env+'". Valid value: dev, dev2, prod');
   }
   return CONF;
 }();
+
+
+function get_api_1c(developer_name) {
+  let api_1C;
+  switch (developer_name) {
+    case 'sasha':
+      api_1C = {
+        ip: '192.168.1.63', // Саша
+        url: '/domovenok/hs/api/v2/',
+        oldAPI: '/domovenok/hs/api/',
+        port: 80,
+        ticket_url: '/domovenok/hs/rq'
+      };
+      break;
+    case 'liza':
+      api_1C = { // Лиза
+        ip: '192.168.1.144',
+        url: '/domovenok/hs/api/v2/',
+        oldAPI: '/domovenok/hs/api/',
+        port: 80,
+        ticket_url: '/domovenok/hs/rq'
+      };
+      break;
+    case 'pasha':
+      api_1C = { // Паша
+        ip: '192.168.1.50',
+        url: '/domovenok/hs/api/v2/',
+        oldAPI: '/domovenok/hs/api/',
+        port: 80,
+        ticket_url: '/domovenok/hs/rq'
+      };
+      break;
+    case 'prod':
+      api_1C = {
+        ip: '192.168.2.4', // prod
+        url: '/domovenok/hs/api/v2/',
+        oldAPI: '/domovenok/hs/api/',
+        port: 80,
+        ticket_url: '/domovenok/hs/rq'
+      };
+      break;
+    default:
+      throw new Error(`Not valid developer_name '${developer_name}'. Valid value: sasha, liza, pasha, prod`);
+  }
+  return api_1C;
+}
