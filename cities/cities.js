e87800330b99c92675c43f55f62c829de1becf10
Dmitrii K <dm-kamaev@rambler.ru>
2018-02-21 10:11:46 +0300

    DEVELOP-2337: add new logic for work with cities and domain

--- cities/cities.js
+++ cities/cities.js
@@ -6,7 +6,13 @@ const config = require('config');
 
 const DEFAULT_CITY_KW = 'moscow';
 
-let CITIES = { DICT : {}, URL: {}};
+const CITIES = { DICT : {}, URL: {}};
+// CITIES.URL: {
+//     spb: 'https://spb.dev2.domovenok.su',
+//     nn: 'https://nn.dev2.domovenok.su',
+//     default: 'https://www.dev2.domovenok.su',
+//     moscow: 'https://www.dev2.domovenok.su'
+// }
 
 async function loadCities() {
     CITIES.LIST = await City.findAll({where:{active:true}})
@@ -20,6 +26,27 @@ async function loadCities() {
     }
     return CITIES.DICT
 }
+
+
+// CITIES.loadCities_new => {
+//  spb: 'https://spb-dev2.domovenok.su',
+//  nn: 'https://nn-dev2.domovenok.su',
+//  default: 'https://www-dev2.domovenok.su',
+//  moscow: 'https://www-dev2.domovenok.su'
+// }
+async function loadCities_new() {
+    CITIES.LIST = await City.findAll({where:{active:true}})
+    for (let city of CITIES.LIST){
+        CITIES.DICT[city.keyword] = city
+        if (city.keyword == DEFAULT_CITY_KW){
+            CITIES.DICT.default = city
+            CITIES.URL.default = config.serverPath.schema + '://' + city.domain + '-' + config.serverPath.domain.withoutCity
+        }
+        CITIES.URL[city.keyword] = config.serverPath.schema + '://' + city.domain + '-' + config.serverPath.domain.withoutCity
+    }
+    return CITIES.DICT
+}
+
 
 module.exports = {
     CITIES,
