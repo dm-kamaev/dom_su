233251ba30166335d1b9b8a6dd0f9b900cdb3984
Dmitrii K <dm-kamaev@rambler.ru>
2018-02-21 10:20:53 +0300

    DEVELOP-2337: generate config for migrate

--- migrate.js
+++ migrate.js
@@ -5,8 +5,17 @@
 const CONF = require('/p/pancake/settings/config.js');
 const child  = require('/p/pancake/my/child.js');
 const list_param  = require('/p/pancake/my/argv.js').get_all();
+const wf_sync = require('/p/pancake/my/wf_sync.js');
 
+const pg = CONF.pg;
 const config_path = '/p/pancake/settings/config_migration.json';
+wf_sync.write('/p/pancake/settings/config_migration.json', JSON.stringify({
+  user: pg.user,
+  password: pg.password,
+  name: pg.database,
+  host: pg.host,
+  port: pg.port
+}, null, 2));
 
 if (!list_param.length) {
   throw new Error('Not exist list_param');
@@ -15,4 +24,4 @@ if (!list_param.length) {
 const cmd = `/p/pancake/node_modules/.bin/node-pg-migrate -f ${config_path} ${list_param.join(' ')}`;
 console.log(cmd);
 
-child.exec(cmd).then(out => console.log(out)).catch(err => console.log(err));
+child.exec(cmd).then(out => console.log(out)).catch(err => console.error(err));
