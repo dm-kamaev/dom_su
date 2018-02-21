ed313a1295fbdb3b5d13ecdda3233e93e7c66dfa
Dmitrii K <dm-kamaev@rambler.ru>
2018-02-21 16:29:54 +0300

    DEVELOP-2800: change api

--- staff/router_employee_photo.js
+++ staff/router_employee_photo.js
@@ -7,20 +7,27 @@ const logger = require('/p/pancake/lib/logger.js');
 
 
 module.exports = function (employee_router) {
-  // /staff/employee_photo/f482eb0b-007b-11e6-80de-00155d594900 dev2
-  // /staff/employee_photo/a2f09a81-8c9b-11e6-80e2-00155d594900 prod
+  // POST /staff/employee_photo/f482eb0b-007b-11e6-80de-00155d594900 dev2
+  // POST /staff/employee_photo/6b759837-c8c2-11e4-944d-002590306b4e dev2
+  // POST /staff/employee_photo/a2f09a81-8c9b-11e6-80e2-00155d594900 prod
   // if not exist photo, then take photo(base64)  from 1C and save to file and redirect to nginx
   // else if photo exist in folder(check db) redirect to nginx
-  employee_router.get('/staff/employee_photo/:file_uuid', async function (ctx) {
+  employee_router.post('/staff/employee_photo/:file_uuid', async function (ctx) {
     try {
       const file_uuid = ctx.params.file_uuid;
       let employee_photo = await db.read_one('SELECT file_name, mime_type FROM employee_photo WHERE file_uuid = $1', [ file_uuid ]);
       const url_path = '/stat/employee_photo/';
       if (employee_photo instanceof Error) {
         throw employee_photo;
-      } else if (employee_photo) {
-        ctx.redirect(url_path+employee_photo.file_name);
-      } else {
+      } else if (employee_photo) { // exist photo on server
+        ctx.status = 200;
+        ctx.body = {
+          ok: false,
+          data: {
+            file_path: url_path+employee_photo.file_name,
+          }
+        };
+      } else { // go to 1C and save to file
         const request1C = new Request1Cv3(null, null, null, ctx);
         // {
         //     "Type":     "png",
@@ -52,12 +59,24 @@ module.exports = function (employee_router) {
             $3
           )`, [ file_uuid, file_name, mime_type ]);
 
-        ctx.redirect(url_path+file_name);
+        ctx.status = 200;
+        ctx.body = {
+          ok: false,
+          data: {
+            file_path: url_path+file_name,
+          }
+        };
       }
     } catch (err) {
       logger.warn(err);
       ctx.status = 500;
-      ctx.body = 'Internal error';
+      ctx.body = {
+        ok: false,
+        error: {
+          code: -1,
+          text: 'Internal error',
+        }
+      };
     }
   });
 };
