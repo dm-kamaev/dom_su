452864707fd9d9f9ed97858186b5c414166c63c8
Dmitrii K <dm-kamaev@rambler.ru>
2018-03-01 16:23:32 +0300

    DEVELOP-2182: add two template

--- staff/router_commodity_material_values.js
+++ staff/router_commodity_material_values.js
@@ -8,8 +8,10 @@ const AuthApi = require('/p/pancake/auth/authApi.js');
 const templates = require('/p/pancake/utils/templates.js');
 const logger = require('/p/pancake/lib/logger.js');
 
-const path_cmd_view= { path: '/p/pancake/staff/templates/mobile/means.html' };
-templates.loadTemplate(path_cmd_view);
+const path_select_means_and_materials= { path: '/p/pancake/staff/templates/mobile/select_means_and_materials.html' };
+templates.loadTemplate(path_select_means_and_materials);
+const show_current_inventory_request= { path: '/p/pancake/staff/templates/mobile/show_current_inventory_request.html' };
+templates.loadTemplate(show_current_inventory_request);
 
 module.exports = function (employee_router) {
   // /staff/f482eb0b-007b-11e6-80de-00155d594900/means_and_materials dev2
@@ -30,7 +32,7 @@ module.exports = function (employee_router) {
         }
       });
       await req_for_1c.do();
-      let {
+      const {
         GetEmployeeData: getEmployeeData,
         'Employee.GetInventoryRequestList': getInventoryRequestList,
         'Employee.InventoryList': Employee_InventoryList
@@ -47,23 +49,29 @@ module.exports = function (employee_router) {
         throw getInventoryRequestList.error;
       }
 
-      var inventory_request = getInventoryRequestList.data.InventoryRequestList[0];
-      if (inventory_request) {
-        await get_data_for_inventory_request(
+      const inventory_request = getInventoryRequestList.data.InventoryRequestList[0];
+      if (inventory_request) { // show view with exist inventory request
+        const info_inventory_request = await view_info_inventory_request(
           new Request1Cv3(token, uuid, null, ctx),
           inventory_request,
           employee_id
         );
+        console.log('HERE === ', info_inventory_request);
+        ctx.status = 200;
+        templateCtx.getEmployeeData = getEmployeeData.data;
+        templateCtx.info_inventory_request = info_inventory_request;
+        ctx.body = templates.getTemplate(show_current_inventory_request)(ctx.proc(templateCtx, ctx));
+      } else { // show view for select means and materials
+        const data = build_data(Employee_InventoryList);
+
+        ctx.status = 200;
+        templateCtx.means = data.means;
+        templateCtx.hash_mean_material = JSON.stringify(data.hash_mean_material);
+        templateCtx.materials = data.materials;
+        templateCtx.getEmployeeData = getEmployeeData.data;
+        ctx.body = templates.getTemplate(path_select_means_and_materials)(ctx.proc(templateCtx, ctx));
       }
 
-      const data = build_data(Employee_InventoryList);
-
-      ctx.status = 200;
-      templateCtx.means = data.means;
-      templateCtx.hash_mean_material = JSON.stringify(data.hash_mean_material);
-      templateCtx.materials = data.materials;
-      templateCtx.getEmployeeData = getEmployeeData.data;
-      ctx.body = templates.getTemplate(path_cmd_view)(ctx.proc(templateCtx, ctx));
     } catch (err) {
       logger.warn(err);
       ctx.status = 500;
@@ -71,8 +79,8 @@ module.exports = function (employee_router) {
     }
   }));
 
-
-  employee_router.post('/staff/:employee_id/new_inventory_request', decorators.login_required, async function (ctx) {
+  // create inventory_request
+  employee_router.post('/staff/:employee_id/inventory_request', decorators.login_required, async function (ctx) {
     const request1Cv3 = new Request1Cv3(new AuthApi(ctx).get_auth_data().token, null, null, ctx);
     await request1Cv3.add('NewInventoryRequest', {
       EmployeeID: ctx.params.employee_id,
@@ -80,11 +88,46 @@ module.exports = function (employee_router) {
       Date: new Date(),
     }).do();
   });
+
+  // close inventory request
+  employee_router.delete('/staff/:employee_id/inventory_request/:inventory_request_id', decorators.login_required, async function (ctx) {
+    let error;
+    try {
+      const { uuid, token } = new AuthApi(ctx).get_auth_data();
+      const request1Cv3 = new Request1Cv3(token, uuid, null, ctx);
+      await request1Cv3.add('CancelInventoryRequest', {
+        InventoryRequestID: ctx.params.inventory_request_id,
+      }).do();
+      const cancel_inventory_request = request1Cv3.get();
+      if (!cancel_inventory_request.ok) {
+        error = cancel_inventory_request.error;
+        throw cancel_inventory_request.error;
+      }
+      ctx.status = 200;
+      ctx.body = {
+        ok: true,
+        data: cancel_inventory_request.data,
+      };
+    } catch (err) {
+      logger.warn(err);
+      ctx.status = 500;
+      ctx.body = {
+        ok: false,
+        error: error || 'Internal error',
+      };
+    }
+  });
 };
 
 
-async function get_data_for_inventory_request(req_for_1c, inventory_request, employee_id) {
-  console.log('inventory_request = ', inventory_request);
+// return ––
+// [{
+//   InventoryTitle: 'Моп 18003 (40*12) абразив',
+//   PackageTitle: 'шт',
+//   Quantity: 1,
+//   Amount: 138
+// }]
+async function view_info_inventory_request(req_for_1c, inventory_request, employee_id) {
   await req_for_1c.add('Employee.GetInventoryRequest', {
     EmployeeID: employee_id,
     InventoryRequestID: inventory_request.InventoryRequestID
@@ -93,8 +136,24 @@ async function get_data_for_inventory_request(req_for_1c, inventory_request, emp
   if (!get_inventory_request.ok) {
     throw get_inventory_request.error;
   }
-
-  return get_inventory_request.data;
+  // get_inventory_request.data –– {
+  //   Date: '2018-02-19T11:18:44Z',
+  //   Status: 'Новая',
+  //   InventoryList: [{
+  //     InventoryTitle: 'Моп 18003 (40*12) абразив',
+  //     PackageTitle: 'шт',
+  //     Quantity: 1,
+  //     Amount: 138
+  //   }]
+  // }
+  return get_inventory_request.data.InventoryList.map(inventory => {
+    return {
+      inventory_title: inventory.InventoryTitle,
+      package_title: inventory.PackageTitle,
+      quantity: inventory.Quantity,
+      amount: inventory.Amount,
+    };
+  });
 }
 
 
