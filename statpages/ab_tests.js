89b185e5b5bd32d193422a23ffea039efe52a047
lobova.a <lobova.a@domovenok.su>
2018-04-10 12:08:41 +0300

    remova ab test for contact page

--- statpages/ab_tests.js
+++ statpages/ab_tests.js
@@ -30,10 +30,6 @@ const ABTestContainer = {
     //     {name: "control", page: 'mite_okon', ratio: 50, description: "Основная"},
     //     {name: "variation", page: 'mite_okon_ab', ratio: 50, description: "Пробуем новый дизайн"}
     // ]},
-    'contacts': { name: "", key: "contacts_1_new_design", forNewUser: true, variations: [
-        {name: "control", page: 'contacts__', ratio: 50, description: "Основная"},
-        {name: "variation", page: 'contacts_ab__', ratio: 50, description: "Пробуем новый дизайн"}
-    ]},
     main: {
       name: "",
       key: "main_9_new",
--- statpages/router_cities/moscow.js
+++ statpages/router_cities/moscow.js
@@ -7,7 +7,6 @@ const moscowTemplates = {
 
     // AB test page
     main_ab: {hide: true, name: 'main_ab.html', data: {menu: {index: true}}},
-    contacts_ab__: {name: 'contacts_ab__.html', ServiceName: 'Контакты', data:{ menu:{contacts: true}}},
     //uborka_ofisov_ab: {name: 'uborka_ofisov_ab.html', ServiceName: 'Уборка офисов', data:{ menu:{legal: true, uborka_ofisov: true}}},
     main_ab2: {hide: true, name: 'main_ab2.html', data: {menu: {index: true}}},
     // price_ab: {name: 'price_ab.html', ServiceName: 'Цены', data:{ menu:{main: true, price: true}}},
