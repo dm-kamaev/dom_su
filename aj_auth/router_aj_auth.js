37a47bfa44d02b1b332ad8994e24a17afdef95e7
Mitya K <dm-kamaev@rambler.ru>
2018-04-11 16:26:51 +0300

    DEVELOP-2674: add pages for critical errors; add log;

--- aj_auth/router_aj_auth.js
+++ aj_auth/router_aj_auth.js
@@ -8,66 +8,16 @@ const logger = require('/p/pancake/lib/logger.js');
 
 const router = module.exports = new Router();
 
-// check user is login or not
-// GET /aj/logged_in
-/* return ––
-{
-  "ok": true,
-  "data": {
-    loggedIn: true
-  }
-}*/
-// TODO(2018.04.10): Remove in future
-// router.get('/aj/logged_in', async function (ctx) {
-//   try {
-//     const authApi = new AuthApi(ctx);
-//     const loggedIn = await authApi.isLoginAsClient() || await authApi.isLoginAsClientEmployee();
-//     const { uuid, client_id, employee_id } = authApi.get_auth_data();
-//     ctx.status = 200;
-//     ctx.body = {
-//       ok: true,
-//       data: {
-//         loggedIn,
-//         u_uuid: uuid,
-//         ClientID: client_id,
-//         EmployeeID: employee_id,
-//       }
-//     };
-//   } catch (err) {
-//     logger.warn(err);
-//     ctx.status = 200;
-//     ctx.body = {
-//       ok: false,
-//       error: {
-//         code: -2,
-//         text: 'Internal error',
-//       }
-//     };
-//   }
-// });
-
 
 // logout
 // GET /aj/logout
 router.get('/aj/logout', async function (ctx) {
-  try {
-    new AuthApi(ctx).logout();
-    ctx.status = 200;
-    ctx.body = {
-      ok: true,
-      data: {
-        Result: true
-      }
-    };
-  } catch (err) {
-    logger.warn(err);
-    ctx.status = 200;
-    ctx.body = {
-      ok: false,
-      error: {
-        code: -2,
-        text: 'Internal error',
-      }
-    };
-  }
+  new AuthApi(ctx).logout();
+  ctx.status = 200;
+  ctx.body = {
+    ok: true,
+    data: {
+      Result: true
+    }
+  };
 });
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,7 +1,14 @@
 'use strict';
 
+// MIDDLEWARe GOR HANDLER ERROR
+
 const { getTemplate, loadTemplate } = require('utils');
 const logger = require('/p/pancake/lib/logger.js');
+const json = require('/p/pancake/my/json.js');
+const AuthApi = require('/p/pancake/auth/authApi.js');
+const Aj_error_internal = require('/p/pancake/errors/Aj_error_internal.js');
+const staff_utils = require('/p/pancake/staff/utils.js');
+const templates = require('/p/pancake/utils/templates.js');
 
 let template404Opt = {
   path: 'templates/error/404.html',
@@ -30,14 +37,64 @@ async function errorMiddleware(ctx, next) {
       errorHtml = getTemplate(template404Opt);
       ctx.body = errorHtml(ctx.proc({}));
     } else {
-      logger.warn(err);
-      ctx.type = 'text/html';
-      ctx.status = 500;
-      errorHtml = getTemplate(template500Opt);
-      ctx.body = errorHtml(ctx.proc({}));
+      const url = ctx.request.url;
+      const authApi = new AuthApi(ctx);
+      // employee or client-employee
+      if (await authApi.isLoginAsClientEmployee()) {
+        await if_error_in_employee_pa(ctx, err, authApi.get_auth_data());
+      // other: client or no auth
+      } else {
+        if (await authApi.isLoginAsClient()) {
+          logger.warn('auth as client url="'+url+'" auth_data='+json.str(authApi.get_auth_data())+' '+err.stack);
+        } else {
+          logger.warn('url="'+url+'" '+err.stack);
+        }
+        ctx.type = 'text/html';
+        ctx.status = 500;
+        errorHtml = getTemplate(template500Opt);
+        ctx.body = errorHtml(ctx.proc({}));
+      }
     }
   }
 }
+
+
+// в личном кабинете сотрудника
+// auth_data –– { uuid: 'user uuid', cleint_id: uuid, employee_id: uuid, token: uuid }
+async function if_error_in_employee_pa(ctx, err, auth_data) {
+  const url = ctx.request.url;
+  const str_auth_data = json.str(auth_data);
+  // if ajax method
+  if (/^\/aj/.test(url)) {
+    const { status, body } = new Aj_error_internal();
+    ctx.status = status;
+    ctx.body = body;
+    logger.warn('AJAX FAILED url="'+url+'" auth_data='+str_auth_data+' '+err.stack);
+    // if html page
+  } else {
+    let name_template;
+    // по идее еще надо проверит,что url начинается со /staff
+    // а то клиент-сотрудник может оказаться в этой секции
+    if (staff_utils.isMobileVersion(ctx)) {
+      logger.warn('EMPLOYEE_PA MOBILE PAGE FAILED url="'+url+'" auth_data='+str_auth_data+' '+err.stack);
+      name_template = staff_utils.get_template('mobile', 'error');
+    } else {
+      logger.warn('EMPLOYEE_PA DESKTOP PAGE FAILED url="'+url+'" auth_data='+str_auth_data+' '+err.stack);
+      name_template = staff_utils.get_template('desktop', 'error');
+    }
+    ctx.type = 'text/html';
+    ctx.status = 500;
+    const templateCtx =  {
+      itIsMe: !ctx.params.EmployeeID || ctx.params.EmployeeID == auth_data.employee_id,
+      employeeId: ctx.params.EmployeeID || auth_data.employee_id,
+      clientId: auth_data.client_id,
+      selfId: auth_data.employee_id,
+      mobileDevice: ctx.userAgent.isMobile,
+    };
+    ctx.body = templates.getTemplate(name_template)(ctx.proc(templateCtx, ctx));
+  }
+}
+
 
 module.exports = {errorMiddleware: errorMiddleware};
 
