63458e81b9d907ce63888eee25d7f1fdaac1af92
Mitya K <dm-kamaev@rambler.ru>
2018-04-12 16:20:37 +0300

    DEVELOP-3153: add 0.5 and 0.33

--- staff/router_commodity_material_values.js
+++ staff/router_commodity_material_values.js
@@ -208,10 +208,10 @@ function build_data(Employee_InventoryList) {
     const package_title = inventory.PackageTitle.replace(/[\d\.]+/g, '').toLowerCase().trim();
 
     const quantity = [{ name: 'Не выбрано', value: 0 }];
-    // if (package_title === 'л') {
-    //   quantity.push({ name: '0.33 л', value: 0.33 });
-    //   quantity.push({ name: '0.5 л', value: 0.5 });
-    // }
+    if (package_title === 'л') {
+      quantity.push({ name: '0.33 л', value: 0.33 });
+      quantity.push({ name: '0.5 л', value: 0.5 });
+    }
     for (var j = 1, l = inventory.Balance; j < l; j++) {
       quantity.push({ name: (j+' '+package_title), value: j });
     }
