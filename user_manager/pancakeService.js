e72b28e4ed83e35144dbdbef35d069b9f7c153ea
Mitya K <dm-kamaev@rambler.ru>
2018-04-18 17:48:26 +0300

    eslint --fix

--- user_manager/pancakeService.js
+++ user_manager/pancakeService.js
@@ -1,134 +1,134 @@
-"use strict";
-const {QueueAsync} = require('./queue')
-const {models, ErrorCodes, ModelsError} = require('models');
+'use strict';
+const {QueueAsync} = require('./queue');
+const {models} = require('models');
 const {User, UTMS, Phone} = models;
-const {saveAndSend} = require('tickets')
+const {saveAndSend} = require('tickets');
 const http = require('http');
 const config = require('config');
-const querystring = require("querystring");
+const querystring = require('querystring');
 
 class PancakeService {
-    constructor(ctx){
-        let self = this
-        this.ctx = ctx
-        this.queue = new QueueAsync(self)
+  constructor(ctx){
+    let self = this;
+    this.ctx = ctx;
+    this.queue = new QueueAsync(self);
+  }
+
+  async setTrackDone(data) {
+    let phone = await Phone.findOne({where: {number: data.phone}});
+    if (phone == null) {
+      throw new Error(`Post call tracking | Phone - ${data.phone} - not found`);
     }
-
-    async setTrackDone(data) {
-        let phone = await Phone.findOne({where: {number: data.phone}})
-        if (phone == null) {
-            throw new Error(`Post call tracking | Phone - ${data.phone} - not found`)
-        }
-        let user = await User.findOne({where: {uuid: phone.user_uuid}})
-        if (user == null){
-            throw new Error(`Post call tracking | On Phone - ${phone.number + ' | User uuid - ' + phone.uuid} - not found`)
-        }
-        let ticket = {
-            channel: data.channel,
-            google_id: user.data.google_id,
-            active: phone.living,
-            user_id: user.uuid,
-        }
-        if (user == null){
-            throw new Error(`Post call tracking | User on Phone - ${data.phone} - not found`)
-        }
-        // {
-        //     "action": "NewOnlineObjects",
-        //     "param": [{
-        //         "type": "NewTrackingCall",
-        //         "data": {
-        //             "channel": 2769574,
-        //             "active": true,
-        //             "user_id": "2e93858f-c0d5-4dfe-af93-d295e805d546",
-        //             "date": "2018-03-05T10:09:25.330Z"
-        //         }
-        //     }]
-        // }
-        this.sendTicket('NewTrackingCall', ticket);
-        this.sendDataGA({
-            'v': 1,
-            'tid': config.analytics.google,
-            'cid': user.data.google_id,
-            't': 'event',
-            'ec': 'call',
-            'ea': 'incoming',
-            'ds': 'call center'
-        })
-
-        // Clean Track Phone Number
-        user.track = {done: true, waiting: false, number: null}
-        this.queue.push(async function (previousResult, pancakeService) {
-            user.set('data.track', user.track);
-            await user.save()
-            await phone.update({living: false})
-            return phone
-        })
+    let user = await User.findOne({where: {uuid: phone.user_uuid}});
+    if (user == null){
+      throw new Error(`Post call tracking | On Phone - ${phone.number + ' | User uuid - ' + phone.uuid} - not found`);
     }
-
-    sendDataGA(data){
-        this.queue.push(async function (previousResult, pancakeService) {
-            let connectParam = {
-                hostname: 'www.google-analytics.com',
-                port: 80,
-                path: "/collect",
-                method: 'POST',
-                headers: {
-                    "User-Agent": "AstDom"
-                }
-            }
-            await pancakeService.sendRequest(connectParam, querystring.stringify(data), 20)
-        })
-    }
-
-    sendTicket(type, data, user_uuid) {
-        this.queue.push(async function (previousResult, pancakeService) {
-            let ticket = await saveAndSend(type, data)
-            return ticket
-        })
-    }
-
-    async getUserUtms(uuid) {
-        let utm_list = await UTMS.findAll({where: {user_uuid: uuid,}})
-        utm_list = utm_list || []
-        return utm_list
+    let ticket = {
+      channel: data.channel,
+      google_id: user.data.google_id,
+      active: phone.living,
+      user_id: user.uuid,
     };
+    if (user == null){
+      throw new Error(`Post call tracking | User on Phone - ${data.phone} - not found`);
+    }
+    // {
+    //     "action": "NewOnlineObjects",
+    //     "param": [{
+    //         "type": "NewTrackingCall",
+    //         "data": {
+    //             "channel": 2769574,
+    //             "active": true,
+    //             "user_id": "2e93858f-c0d5-4dfe-af93-d295e805d546",
+    //             "date": "2018-03-05T10:09:25.330Z"
+    //         }
+    //     }]
+    // }
+    this.sendTicket('NewTrackingCall', ticket);
+    this.sendDataGA({
+      'v': 1,
+      'tid': config.analytics.google,
+      'cid': user.data.google_id,
+      't': 'event',
+      'ec': 'call',
+      'ea': 'incoming',
+      'ds': 'call center'
+    });
 
-    //async check404
+    // Clean Track Phone Number
+    user.track = {done: true, waiting: false, number: null};
+    this.queue.push(async function (previousResult, pancakeService) {
+      user.set('data.track', user.track);
+      await user.save();
+      await phone.update({living: false});
+      return phone;
+    });
+  }
 
-    sendRequest(connectParam, body, timeout){
-        timeout = timeout || 20
-        let response = ''
-        if (connectParam.method == 'POST'){
-            connectParam.headers['Content-length'] = Buffer.from(body).length;
+  sendDataGA(data){
+    this.queue.push(async function (previousResult, pancakeService) {
+      let connectParam = {
+        hostname: 'www.google-analytics.com',
+        port: 80,
+        path: '/collect',
+        method: 'POST',
+        headers: {
+          'User-Agent': 'AstDom'
         }
-        return new Promise((reslove, reject) => {
-            let req = http.request(connectParam, (res) => {
-                res.setEncoding('utf8')
-                res.on('data', (chunk) => {
-                    response += chunk
-                });
-                res.on('end', () => {
-                    reslove(response);
-                });
-            })
-            req.setTimeout(1000 * timeout, function () {
-                reject(new Error(`Request timeout error - ${connectParam}`))
-            })
-            req.on('error', (e) => {
-                reject(new Error(`The request ended in failure - ${connectParam}`))
-            })
-            if (connectParam.method == 'POST'){
-                req.write(body)
-            }
-            req.end()
-        })
-    }
+      };
+      await pancakeService.sendRequest(connectParam, querystring.stringify(data), 20);
+    });
+  }
 
-    runAsyncTask() {
-        this.queue.do()
+  sendTicket(type, data, user_uuid) {
+    this.queue.push(async function (previousResult, pancakeService) {
+      let ticket = await saveAndSend(type, data);
+      return ticket;
+    });
+  }
+
+  async getUserUtms(uuid) {
+    let utm_list = await UTMS.findAll({where: {user_uuid: uuid,}});
+    utm_list = utm_list || [];
+    return utm_list;
+  }
+
+  //async check404
+
+  sendRequest(connectParam, body, timeout){
+    timeout = timeout || 20;
+    let response = '';
+    if (connectParam.method == 'POST'){
+      connectParam.headers['Content-length'] = Buffer.from(body).length;
     }
+    return new Promise((reslove, reject) => {
+      let req = http.request(connectParam, (res) => {
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+          response += chunk;
+        });
+        res.on('end', () => {
+          reslove(response);
+        });
+      });
+      req.setTimeout(1000 * timeout, function () {
+        reject(new Error(`Request timeout error - ${connectParam}`));
+      });
+      req.on('error', (e) => {
+        reject(new Error(`The request ended in failure - ${connectParam}`));
+      });
+      if (connectParam.method == 'POST'){
+        req.write(body);
+      }
+      req.end();
+    });
+  }
+
+  runAsyncTask() {
+    this.queue.do();
+  }
 }
 
 module.exports = {
-    PancakeService
-}
+  PancakeService
+};
