28018a288739e965cdebcbc4c5b5daa279b602c4
Mitya K <dm-kamaev@rambler.ru>
2018-04-19 17:12:10 +0300

    DEVELOP-2781: call method for set applicant phone

--- user_manager/middleware/callTracking.js
+++ user_manager/middleware/callTracking.js
@@ -1,14 +1,18 @@
-"use strict";
+'use strict';
 
 async function callTracking(ctx, next) {
-    if (ctx.state.pancakeUser.checkTrackNeed()){
-        ctx.state.pancakeUser.setTrackWaiting(true)
-        await ctx.state.pancakeUser.setTrackNumber()
-    } else {
-        ctx.state.pancakeUser.setTrackWaiting(false)
-
-    }
-    await next()
+  const user = ctx.state.pancakeUser;
+  console.log('callTracking => user.checkTrackNeed ===', user.checkTrackNeed());
+  if (user.checkTrackNeed()) {
+    user.setTrackWaiting(true);
+    await user.set_track_number_for_client();
+    await user.set_track_number_for_applicant();
+  } else {
+    user.setTrackWaiting(false);
+  }
+  await next();
 }
 
-module.exports = {callTracking}
+module.exports = {
+  callTracking
+};
