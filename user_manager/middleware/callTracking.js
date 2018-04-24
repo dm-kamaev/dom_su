fa09262b62fe62bbcb6cb99e97c303e246d7158d
Mitya K <dm-kamaev@rambler.ru>
2018-04-24 12:52:57 +0300

    DEVELOP-2781: change format

--- user_manager/middleware/callTracking.js
+++ user_manager/middleware/callTracking.js
@@ -2,7 +2,6 @@
 
 async function callTracking(ctx, next) {
   const user = ctx.state.pancakeUser;
-  console.log('callTracking => user.checkTrackNeed ===', user.checkTrackNeed());
   if (user.checkTrackNeed()) {
     user.setTrackWaiting(true);
     await user.set_track_number_for_client();
