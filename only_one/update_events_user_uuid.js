78af8979746d674973b3a1a4fa9fc1c19c4ecf51
wwwdomovenoksu <domovenoksu>
2018-06-01 11:39:50 +0300

    DEVELOP-3491: script for extract user_uuid

--- only_one/update_events_user_uuid.js
+++ only_one/update_events_user_uuid.js
@@ -9,11 +9,11 @@ const promise_api = require('/p/pancake/my/promise_api.js');
 
 
 
-const NUMBER_ELS = 1000;
+const NUMBER_ELS = 100000;
 
 void async function () {
 
-  const stream = new db.Stream_via_cursor('SELECT uuid, data FROM events_2017_2018');
+  const stream = new db.Stream_via_cursor('SELECT uuid, data, user_uuid, its_robot FROM events_2017_2018');
 
   await next(stream, await stream.get(NUMBER_ELS));
 
@@ -29,8 +29,10 @@ async function next(stream, rows) {
   }
   rows = filter_rows(rows);
   if (rows) {
-    await mark_event_user_bots(rows);
-    await timeout();
+    await timeout(33);
+    await update_events_user_id(rows);
+  } else {
+    await timeout(10);
   }
   rows = null;
   // if (i > 10) {
@@ -43,57 +45,55 @@ async function next(stream, rows) {
 
 function filter_rows(rows) {
   rows = rows.filter(function(row) {
-    if (!row.data) {
+    if (row.user_uuid) {
       return false;
     }
-    if (!row.data.headers) {
+    const data = row.data;
+    if (!data) {
       return false;
     }
-    if (!row.data.headers.cookie) {
+    if (!data.headers) {
+      return false;
+    }
+    if (!data.headers.cookie) {
       return false;
     }
 
-    const user_agent = row.data.headers['user-agent'];
-    if (!user_agent) {
+    const cookies = parse_cookie.parse(data.headers.cookie);
+    if (!(cookies.u_uuid || cookies.session_uid_dom)) {
       return false;
     }
 
-    if (/bot/ig.test(user_agent) || /1C/ig.test(user_agent) || /Go|Python|Perl|php|Java/ig.test(user_agent)) {
-      return true;
-    }
-
-    return false;
+    return true;
   });
 
   rows = rows.map(({ uuid, data }) => {
     const cookies = parse_cookie.parse(data.headers.cookie);
-    const user_agent = data.headers['user-agent'];
     return {
       uuid,
       user_uuid: cookies.u_uuid || cookies.session_uid_dom,
-      user_agent,
-    }
+    };
   });
   if (rows.length) {
-    console.log(rows);
+    console.log('filter_rows=', rows);
     // global.process.exit();
   }
   return (rows.length) ? rows : null;
 }
 
 
-function timeout() {
+function timeout(sec) {
   return new Promise((resolve, reject) => {
     setTimeout(function() {
       resolve();
-    }, 40000);
+    }, sec * 1000);
   });
 }
 
 
-async function mark_event_user_bots(rows) {
+async function update_events_user_id(rows) {
   await promise_api.queue(rows, async function ({ uuid, user_uuid }) {
-    await db.edit(`UPDATE users SET its_robot = true WHERE uuid = '${user_uuid}'`);
-    await db.edit(`UPDATE events_2017_2018 SET its_robot = true WHERE uuid = '${uuid}'`);
+    await db.edit(`UPDATE events_2017_2018 SET user_uuid = '${user_uuid}' WHERE uuid = '${uuid}'`);
+    await timeout(2);
   });
 }
