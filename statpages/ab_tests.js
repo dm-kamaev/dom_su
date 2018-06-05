254a89372157d510173e948fdda6f261a30d8f2a
lobova.a <lobova.a@domovenok.su>
2018-06-05 13:41:26 +0300

    stop ab test podderzhka

--- statpages/ab_tests.js
+++ statpages/ab_tests.js
@@ -5,23 +5,23 @@ const ABTestContainer = {
     // 'generalnaya_uborka': { name: "Тестирование 3-го шага на рассчете стоимости", key: "generalnaya_uborka_1_3step", forNewUser: true, variations: [
     //     {name: "control", page: 'generalnaya_uborka', ratio: 50, description: "Основные дополнительные услуги"},
     //     {name: "variation", page: 'generalnaya_uborka_ab', ratio: 50, description: "Мытье окон как доп. услуга"},
-    // ]},
-    podderzhka: {
-      name: "",
-      key: "podderzhka_9_order",
-      forNewUser: true,
-      variations: [{
-        name: "control",
-        page: 'podderzhka',
-        ratio: 50,
-        description: "Основная"
-      }, {
-        name: "variation",
-        page: 'podderzhka_ab',
-        ratio: 50,
-        description: "Форма первая"
-      }]
-    },
+    // // ]},
+    // podderzhka: {
+    //   name: "",
+    //   key: "podderzhka_9_order",
+    //   forNewUser: true,
+    //   variations: [{
+    //     name: "control",
+    //     page: 'podderzhka',
+    //     ratio: 50,
+    //     description: "Основная"
+    //   }, {
+    //     name: "variation",
+    //     page: 'podderzhka_ab',
+    //     ratio: 50,
+    //     description: "Форма первая"
+    //   }]
+    // },
     // 'posle_remonta': { name: "", key: "posle_remonta_2_new_design", forNewUser: true, variations: [
     //     {name: "control", page: 'posle_remonta', ratio: 50, description: "Основная"},
     //     {name: "variation", page: 'posle_remonta_ab', ratio: 50, description: "Пробуем новый дизайн"}
