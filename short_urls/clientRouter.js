fb691716494d8a313e07d96c7bfb3dfe2cc3168b
Mitya K <dm-kamaev@rambler.ru>
2018-07-04 12:32:37 +0000

    DEVELOP-3805: show page via redirect

--- short_urls/clientRouter.js
+++ short_urls/clientRouter.js
@@ -5,6 +5,7 @@ const Router = require('koa-router');
 
 const clientShortUrlRouter = new Router();
 
+// /s/2wtZbS
 clientShortUrlRouter.get('/s/:key', async function(ctx, next) {
   const shortUrl = await ShortUrl.findOne({
     where: {
@@ -21,15 +22,18 @@ clientShortUrlRouter.get('/s/:key', async function(ctx, next) {
         });
       }
     }
+    // render page for open in 1c
     if (shortUrl.url && shortUrl.url.startsWith('e1c')) {
-      // let iframe = `<iframe src="${shortUrl.url}"></iframe><script>close();</script>`
-      // ctx.type = 'text/html; charset=utf-8';
-      // ctx.body = `<html><head></head><body>${iframe} <script>${js}</script></body></html>`;
+
       ctx.type = 'text/html; charset=utf-8';
       ctx.body = h_open_link_1c(shortUrl.url);
     } else {
-      ctx.status = 301;
-      ctx.redirect(shortUrl.url);
+      // ctx.status = 301;
+      // ctx.redirect(shortUrl.url);
+
+      // render page with text about for redirect to client_pa
+      ctx.status = 200;
+      ctx.body = render_page_for_redirect_to_client_pa();
     }
   }
   await next();
@@ -64,6 +68,35 @@ function h_open_link_1c(url) {
   return html;
 }
 
+
+function render_page_for_redirect_to_client_pa() {
+  const html = `
+    <!DOCTYPE html>
+      <html lang="RU">
+      <head>
+        <meta charset="UTF-8"/>
+        <title>Привяжите карты</title>
+        <style>
+          html{margin:0;padding:0;color:#333;hyphens:auto;-webkit-hyphens:auto;-moz-hyphens:auto;-ms-hyphens:auto;}
+          body {font-family:Helvetica,Arial,sans-serif;font-size:120%;margin:0}
+          a{color:#1A0DAB;text-decoration:underline;}
+          a:active, a:focus, img{outline:0}
+          a:visited{color:#1A0DAB}
+          a:hover{color:#FF6633}
+          p{margin:0;padding:0;line-height:1.5}
+        </style>
+      </head>
+      <body>
+        <div style="margin:100px auto 0 auto;text-align:center">
+          <p style="margin:0 auto">К сожалению, ссылка устарела.</p>
+          <p style="margin:0 auto">Перейдите в личный кабинет для <a href="/private/profile">оплаты</a>.</p>
+          <p style="margin:0 auto">Приносим свои извинения.</p>
+        </div>
+      </body>
+    </html>
+  `;
+  return html;
+}
 
 module.exports = {
   clientShortUrlRouter
