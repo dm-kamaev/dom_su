144d00f9122336b92d8ce1ca6b51a4c1acfcf5dd
Mitya K <dm-kamaev@rambler.ru>
2018-07-11 13:04:27 +0000

    remove exit

--- proxyRequestTo1C/routerProxyRequestTo1C.js
+++ proxyRequestTo1C/routerProxyRequestTo1C.js
@@ -119,9 +119,7 @@ router.post('/proxy_request/:methodName', check_auth.ajax(async function (ctx) {
     const client_id = auth_data.client_id;
     body.ClientID = client_id;
   } else if (method_name === 'Client.SetOrderReview') {
-    console.dir('=== Client.SetOrderReview ===', { depth: 20, colors: true });
     await review_store.create_review(ctx, body);
-    global.process.exit();
   }
 
   const request1C = new Request1Cv3(user.auth1C.token, user.uuid, null, ctx);
