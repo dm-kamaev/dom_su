5357e604e77763eb4e96404fa99ff1b392e047b5
lobova.a <lobova.a@domovenok.su>
2018-08-01 11:47:26 +0300

    change vakancii

--- statpages/router_cities/moscow.js
+++ statpages/router_cities/moscow.js
@@ -6,7 +6,7 @@ const moscowTemplates = {
 
 
     // AB test page
-    main_ab: {hide: true, name: 'main_ab.html', data: {menu: {index: true}}},
+    //main_ab: {hide: true, name: 'main_ab.html', data: {menu: {index: true}}},
     //uborka_ofisov_ab: {name: 'uborka_ofisov_ab.html', ServiceName: 'Уборка офисов', data:{ menu:{legal: true, uborka_ofisov: true}}},
     // main_ab2: {hide: true, name: 'main_ab2.html', data: {menu: {index: true}}},
     // price_ab: {name: 'price_ab.html', ServiceName: 'Цены', data:{ menu:{main: true, price: true}}},
